76a7f346b3166f487bb5660d19ecd8d47aa8267a
Kyle Laplante <kyle.laplante@gmail.com>
2012-11-26 23:22:00 -0800

    more compatible

--- ui.c
+++ ui.c
@@ -273,7 +273,8 @@ static void draw_text_line(int row, const char* t, int align) {
 }
 
 //#define MENU_TEXT_COLOR 255, 160, 49, 255
-#define MENU_TEXT_COLOR 0, 191, 255, 255
+//#define MENU_TEXT_COLOR 0, 191, 255, 255 //blue
+#define MENU_TEXT_COLOR 1, 1, 1, 255 //black
 #define NORMAL_TEXT_COLOR 200, 200, 200, 255
 #define HEADER_TEXT_COLOR NORMAL_TEXT_COLOR
 
